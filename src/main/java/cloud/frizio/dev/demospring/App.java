f9e12902903850f6b1c9a7364ac7f133e41baa28
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-06 12:12:45 +0100

    First spring boot appication with context

--- src/main/java/cloud/frizio/dev/demospring/App.java
+++ src/main/java/cloud/frizio/dev/demospring/App.java
@@ -1,11 +1,17 @@
 package cloud.frizio.dev.demospring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-    public static void main( String[] args ) {
-        // Create the object
-        Coach theCoach = new TrackCoach();
-        // Use the object
-        System.out.println(theCoach.getDailyWorkout());
-    }
+  public static void main(String[] args) {
+    // Load the spring configuration file
+    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    // Retrieve bean from spring container
+    Coach theCoach = context.getBean("myCoach", Coach.class);
+    // Call methods on the bean
+    System.out.println(theCoach.getDailyWorkout());
+    // Close the context
+    context.close();
+  }
+
 }
