945d9e2199cb9ad1c7de111797f9653198effa5f
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-06 12:27:22 +0100

    Add third implementation of the Coach interface

---
+++ src/main/java/cloud/frizio/dev/demospring/FootballCoach.java
@@ -0,0 +1,14 @@
+package cloud.frizio.dev.demospring;
+
+/**
+ * FootballCoach
+ */
+public class FootballCoach implements Coach {
+
+  @Override
+  public String getDailyWorkout() {
+    return "Run Run Run for 1000 yard!!!";
+  }
+
+  
+}
