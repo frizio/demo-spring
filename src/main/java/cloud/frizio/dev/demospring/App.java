b7ef03ba969eab57e1ed897a14b05c15e513d5a5
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-25 19:16:51 +0100

    Update dependencies and add statemachine

--- src/main/java/cloud/frizio/dev/demospring/App.java
+++ src/main/java/cloud/frizio/dev/demospring/App.java
@@ -1,8 +1,10 @@
 package cloud.frizio.dev.demospring;
 
+import cloud.frizio.dev.demospring.statemachine.order.automata.OrderEvents;
+import cloud.frizio.dev.demospring.statemachine.order.automata.OrderStates;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
+  public static void main( String[] args ) {
+      System.out.println( "Demo Spring Statemachine" );
+  }
 }
