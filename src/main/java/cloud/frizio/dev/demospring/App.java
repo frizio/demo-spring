6f91ca97d790228b1ec7ec977964adf1c4b85aea
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-27 19:16:36 +0100

    Add AnnotationConfigApplicationContext in main

--- src/main/java/cloud/frizio/dev/demospring/App.java
+++ src/main/java/cloud/frizio/dev/demospring/App.java
@@ -1,12 +1,30 @@
 package cloud.frizio.dev.demospring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import cloud.frizio.dev.demospring.statemachine.order.BeanConfig;
+import cloud.frizio.dev.demospring.statemachine.order.OrderManager;
 import cloud.frizio.dev.demospring.statemachine.order.automata.OrderEvents;
 import cloud.frizio.dev.demospring.statemachine.order.automata.OrderStates;
+import cloud.frizio.dev.demospring.statemachine.order.automata.PurchaseOrderConfig;
 
 public class App {
   public static void main( String[] args ) {
-      System.out.println( "Demo Spring Statemachine" );
-      System.out.println(OrderStates.CREATED + ": " + OrderStates.CREATED.getDescription());
-      System.out.println(OrderEvents.APPROVE + ": " + OrderEvents.APPROVE.getDescription());
+    System.out.println( "Demo Spring Statemachine" );
+    System.out.println(OrderStates.CREATED + ": " + OrderStates.CREATED.getDescription());
+    System.out.println(OrderEvents.APPROVE + ": " + OrderEvents.APPROVE.getDescription());
+
+    AnnotationConfigApplicationContext context = 
+        new AnnotationConfigApplicationContext(
+              PurchaseOrderConfig.class,
+              BeanConfig.class
+            );
+
+    OrderManager orderManager = context.getBean(OrderManager.class);
+
+
+    
+    context.close();
+
   }
 }
